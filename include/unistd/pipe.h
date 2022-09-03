01575c21854718704cc1751a361492e8476b181a
Jason Stephenson <jason@sigio.com>
2022-09-02 20:56:26 -0400

    Update LICENSE reference in source headers.
    Signed-off-by: Jason Stephenson <jason@sigio.com>

--- include/unistd/pipe.h
+++ include/unistd/pipe.h
@@ -2,7 +2,7 @@
 // Copyright © 2022 Jason Stephenson
 //
 // Distributed under the Boost Software License, Version 1.0.
-// See accompanying file LICENSE_1_0.txt or copy at https://www.boost.org/LICENSE_1_0.txt
+// See accompanying file LICENSE or copy at https://www.boost.org/LICENSE_1_0.txt
 #ifndef UNISTD_PIPE_H
 #define UNISTD_PIPE_H
 #include <array>
--- src/host.cpp
+++ src/host.cpp
@@ -1,7 +1,7 @@
 // Copyright © 2022 Jason Stephenson
 //
 // Distributed under the Boost Software License, Version 1.0.
-// See accompanying file LICENSE_1_0.txt or copy at https://www.boost.org/LICENSE_1_0.txt
+// See accompanying file LICENSE or copy at https://www.boost.org/LICENSE_1_0.txt
 #include <unistd/host.h>
 #include <unistd/err.h>
 #include <array>
